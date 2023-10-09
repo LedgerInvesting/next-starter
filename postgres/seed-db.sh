e12b24479467e23ea5f8cbd28e81fc47627b9c08
Aymeric <aymeric.rabot@gmail.com>
2023-10-09 15:41:46 -0400

    Update seed-db.sh

--- postgres/seed-db.sh
+++ postgres/seed-db.sh
@@ -3,8 +3,14 @@
 # Load environment variables from .env file
 source ../.env.local
 
+# Check if a seed file is provided
+if [[ -z "$1" ]]; then
+  echo "📂 Seed file needed."
+  exit 1
+fi
+
 # Export data from the local database
 echo "🏄‍♂️ Import to local database..."
-psql "$DATABASE_URL" < seed.sql
+psql "$DATABASE_URL" < "$1"
 
 echo "✅ Import completed!"
